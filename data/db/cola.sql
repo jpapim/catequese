113229b7d98a15585f3fa80aa92fd19ae3ae0c2d
Alysson Vicuna <alysson.oliveira@cultura.gov.br>
2016-05-09 16:46:11 -0300

    [Novo] Atualizando o Script Inicial do Banco de Dados, eliminando as tabelas não utilizadas no sistema. [Novo] Atualizando O MER para que contemple todas as modificações realizadas na base Fisica. [Novo] Atualizando a View AUTH para deixar de referenciar a tabela de contratos.

--- data/db/cola.sql
+++ data/db/cola.sql
@@ -1,7 +1,1 @@
-mysqldump -u root -B competicoestkd -p > E:\wamp\www\competicaotkd\data\db\dump_tkd.sql
-
-mysqldump -u root -B competicoestkd -p > E:\wamp\www\competicaotkd\data\db\script_inicial.sql
-
-
-
-
+mysqldump -u root -B bdcatequese -p > C:\xampp\htdocs\catequese\data\db\script_inicial.sql
