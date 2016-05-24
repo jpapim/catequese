48ae888d7a992100ecc09b6fe0920d5f011e9004
Igor Oliveira <igor.oliveira.m@gmail.com>
2016-05-24 17:29:07 -0300

    *[Novo] Categoria Peso  com a implementação da Index-Pagination

--- module/CategoriaPeso/src/CategoriaPeso/Controller/CategoriaPesoController.php
+++ module/CategoriaPeso/src/CategoriaPeso/Controller/CategoriaPesoController.php
@@ -3,6 +3,10 @@
 namespace CategoriaPeso\Controller;
 
 use Estrutura\Controller\AbstractCrudController;
+use Estrutura\Helpers\Cript;
+use Estrutura\Helpers\Data;
+use Zend\View\Model\ViewModel;
+use Zend\View\Model\JsonModel;
 
 class CategoriaPesoController extends AbstractCrudController
 {
@@ -24,6 +28,46 @@ class CategoriaPesoController extends AbstractCrudController
     {
         return parent::index($this->service, $this->form);
     }
+    
+    public function indexPaginationAction()
+    {
+        //http://igorrocha.com.br/tutorial-zf2-parte-9-paginacao-busca-e-listagem/4/
+        
+        $filter = $this->getFilterPage();
+
+        $camposFilter = [
+            '0' => [
+                'filter' => "categoria_peso.nm_categoria_peso LIKE ?",
+            ],
+            
+            '6' => NULL,
+        ];
+        $paginator = $this->service->getCatPesoPaginator($filter, $camposFilter);
+
+        $paginator->setItemCountPerPage($paginator->getTotalItemCount());
+
+        $countPerPage = $this->getCountPerPage(
+                current(\Estrutura\Helpers\Pagination::getCountPerPage($paginator->getTotalItemCount()))
+        );
+
+        $paginator->setItemCountPerPage($this->getCountPerPage(
+                        current(\Estrutura\Helpers\Pagination::getCountPerPage($paginator->getTotalItemCount()))
+        ))->setCurrentPageNumber($this->getCurrentPage());
+
+        $viewModel = new ViewModel([
+            'service' => $this->service,
+            'form' => $this->form,
+            'paginator' => $paginator,
+            'filter' => $filter,
+            'countPerPage' => $countPerPage,
+            'camposFilter' => $camposFilter,
+            'controller' => $this->params('controller'),
+            'atributos' => array()
+        ]);
+
+        return $viewModel->setTerminal(TRUE);
+    }
+    
 
     public function gravarAction(){
         #Alysson
