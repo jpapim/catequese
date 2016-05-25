b30a5518ea48c0a48c5e1178b86e3b3a2a8ae26f
Eduferr <eduardofgsantos@gmail.com>
2016-05-24 21:51:43 -0300

    [Fix] Corrigindo funcionanmento do gitignore

--- module/Graduacao/src/Graduacao/Controller/GraduacaoController.php
+++ module/Graduacao/src/Graduacao/Controller/GraduacaoController.php
@@ -24,8 +24,7 @@ class GraduacaoController extends AbstractCrudController
         parent::init();
     }
 
-    public function indexAction()
-    {
+    public function indexAction(){
         $dadosView = [
             'service' => $this->service,
             'form' => $this->form,
@@ -37,16 +36,65 @@ class GraduacaoController extends AbstractCrudController
         //return parent::index($this->service, $this->form);
     }
 
+
+    public function indexPaginationAction()
+    {
+        //http://igorrocha.com.br/tutorial-zf2-parte-9-paginacao-busca-e-listagem/4/
+
+        $filter = $this->getFilterPage();
+
+        $camposFilter = [
+            '0' => [
+                'filter' => "arte_marcial.nm_arte_marcial LIKE ?",
+            ],
+            '1' => [
+                'filter' => "estilos.nm_estilo LIKE ?",
+            ],
+            '2' => [
+                'filter' => "graduacoes.nm_graduacao LIKE ?",
+            ],
+            '3' => NULL,
+        ];
+
+
+        $graduacaoPaginator = $this->service->getGraduacaoPaginator($filter, $camposFilter);
+
+        $graduacaoPaginator->setItemCountPerPage($graduacaoPaginator->getTotalItemCount());
+
+        $countPerPage = $this->getCountPerPage(
+            current(\Estrutura\Helpers\Pagination::getCountPerPage($graduacaoPaginator->getTotalItemCount()))
+        );
+
+        $graduacaoPaginator->setItemCountPerPage($this->getCountPerPage(
+            current(\Estrutura\Helpers\Pagination::getCountPerPage($graduacaoPaginator->getTotalItemCount()))
+        ))->setCurrentPageNumber($this->getCurrentPage());
+
+        $viewModel = new ViewModel([
+            'service' => $this->service,
+            'form' => $this->form,
+            'paginator' => $graduacaoPaginator,
+            'filter' => $filter,
+            'countPerPage' => $countPerPage,
+            'camposFilter' => $camposFilter,
+            'controller' => $this->params('controller'),
+            'atributos' => array()
+        ]);
+
+        return $viewModel->setTerminal(TRUE);
+    }
+
+
+
+
     public function gravarAction() {
         #Alysson
         $controller = $this->params('controller');
         $this->addSuccessMessage('Registro Alterado com sucesso');
-        $this->redirect()->toRoute('navegacao', array('controller' => $controller, 'action' => 'index'));
+        $this->redirect()->toRoute('navegacao', array('controller' => $controller, 'action' => 'index-pagination'));
         return parent::gravar($this->service, $this->form);
     }
 
-    public function cadastroAction()
-    {
+    public function cadastroAction(){
         return parent::cadastro($this->service, $this->form);
     }
 
@@ -55,12 +103,5 @@ class GraduacaoController extends AbstractCrudController
         return parent::excluir($this->service, $this->form);
     }
 
-    public function xxxAction()
-    {
-        $dadosView = array(
-                            'abc'=>'wwwwwwwwwwwwwwwwwwwwwwwwwwwwww',
-                            'def'=>'qqqqqqqqqqqqqqqqqqqqqqqqqqqqqq'
-        );
-        return new ViewModel($dadosView);
-    }
+    
 }
--- module/Graduacao/src/Graduacao/Service/GraduacaoService.php
+++ module/Graduacao/src/Graduacao/Service/GraduacaoService.php
@@ -6,6 +6,20 @@ use \Graduacao\Entity\GraduacaoEntity as Entity;
 
 class GraduacaoService extends Entity{
 
+    public function fetchAllGraduacao($params) {
+
+        $resultSet = NULL;
+
+        if (isset($params['id_graduacao']) && $params['id_graducao']) {
+
+            $resultSet = $this->select(
+                [
+                    'graduacoes.id_graduacao = ? ' => $params['id_graduacao']
+                ]
+            );
+        }
+        return $resultSet;    }
+
     public function getGraduacaoToArray($id) {
 
         $sql = new \Zend\Db\Sql\Sql($this->getAdapter());
@@ -18,52 +32,102 @@ class GraduacaoService extends Entity{
         return $sql->prepareStatementForSqlObject($select)->execute()->current();
     }
 
-/*
-    public function getGraduacao($id) {
+    public function getFiltrarGraduacaoPorNomeToArray($nm_graduacao) {
 
         $sql = new \Zend\Db\Sql\Sql($this->getAdapter());
 
-        die($id);
-        $select = $sql->select('usuario')
-            ->join(
-                'contrato', 'contrato.id_usuario = usuario.id_usuario'
-            )
-            ->join(
-                'conta_bancaria', 'conta_bancaria.id_usuario = usuario.id_usuario', \Zend\Db\Sql\Select::SQL_STAR, \Zend\Db\Sql\Select::JOIN_LEFT
-            )
-            ->join(
-                'estado_civil', 'estado_civil.id_estado_civil = usuario.id_estado_civil', \Zend\Db\Sql\Select::SQL_STAR, \Zend\Db\Sql\Select::JOIN_LEFT
-            )
-            ->join(
-                'situacao_usuario', 'situacao_usuario.id_situacao_usuario = usuario.id_situacao_usuario'
-            )
-            ->join(
-                'email', 'email.id_email = usuario.id_email'
-            )
-            ->join(
-                'telefone', 'telefone.id_telefone = usuario.id_telefone'
-            )
-            ->join(
-                'endereco', 'endereco.id_endereco = usuario.id_endereco', \Zend\Db\Sql\Select::SQL_STAR, \Zend\Db\Sql\Select::JOIN_LEFT
-            )
-            ->join(
-                'cidade', 'cidade.id_cidade = endereco.id_cidade', \Zend\Db\Sql\Select::SQL_STAR, \Zend\Db\Sql\Select::JOIN_LEFT
-            )
-            ->join(
-                'estado', 'estado.id_estado = cidade.id_estado', \Zend\Db\Sql\Select::SQL_STAR, \Zend\Db\Sql\Select::JOIN_LEFT
-            )
-            ->join(
-                'banco', 'banco.id_banco = conta_bancaria.id_banco', \Zend\Db\Sql\Select::SQL_STAR, \Zend\Db\Sql\Select::JOIN_LEFT
-            )
-            ->join(
-                'tipo_conta', 'tipo_conta.id_tipo_conta = conta_bancaria.id_tipo_conta', \Zend\Db\Sql\Select::SQL_STAR, \Zend\Db\Sql\Select::JOIN_LEFT
-            )
+        $select = $sql->select('graduacoes')
+            ->columns(array('nm_graduacao', 'id_academia')) #Colunas a retornar. Basta Omitir que ele traz todas as colunas
             ->where([
-                'usuario.id_usuario = ?' => $id,
+                "graduacoes.nm_graduacao LIKE ?" => '%' . $nm_graduacao . '%',
             ]);
-        print_r($sql->prepareStatementForSqlObject($select)->execute());exit;
 
-        return $sql->prepareStatementForSqlObject($select)->execute()->current();
+        return $sql->prepareStatementForSqlObject($select)->execute();
     }
-*/
+
+    public function fetchPaginator($pagina = 1, $itensPagina = 5, $ordem = 'nm_graduacao ASC', $like = null, $itensPaginacao = 5) {
+        //http://igorrocha.com.br/tutorial-zf2-parte-9-paginacao-busca-e-listagem/4/
+        // preparar um select para tabela contato com uma ordem
+        $sql = new \Zend\Db\Sql\Sql($this->getAdapter());
+        $select = $sql->select('graduacao')->order($ordem);
+
+        if (isset($like)) {
+            $select
+                ->where
+                ->like('id_graduacao', "%{$like}%")
+                ->or
+                ->like('nm_graducao', "%{$like}%")
+                #->or
+                #->like('telefone_principal', "%{$like}%")
+                #->or
+                #->like('data_criacao', "%{$like}%")
+            ;
+        }
+
+        // criar um objeto com a estrutura desejada para armazenar valores
+        $resultSet = new HydratingResultSet(new Reflection(), new \graduacao\Entity\GraduacaoEntity());
+
+        // criar um objeto adapter paginator
+        $paginatorAdapter = new DbSelect(
+        // nosso objeto select
+            $select,
+            // nosso adapter da tabela
+            $this->getAdapter(),
+            // nosso objeto base para ser populado
+            $resultSet
+        );
+
+        # var_dump($paginatorAdapter);
+        #die;
+        // resultado da pagina��o
+        return (new Paginator($paginatorAdapter))
+            // pagina a ser buscada
+            ->setCurrentPageNumber((int) $pagina)
+            // quantidade de itens na página
+            ->setItemCountPerPage((int) $itensPagina)
+            ->setPageRange((int) $itensPaginacao);
+    }
+
+
+    public function getGraduacaoPaginator($filter = NULL, $camposFilter = NULL) {
+
+        $sql = new \Zend\Db\Sql\Sql($this->getAdapter());
+
+        $select = $sql->select('graduacoes')->columns([
+            'id_graduacao',
+            'nm_graduacao',
+
+        ])
+            ->join('arte_marcial', 'arte_marcial.id_arte_marcial = graduacoes.id_arte_marcial', [
+                'nm_arte_marcial'
+            ])
+            ->join('estilos', 'estilos.id_estilo = graduacoes.id_estilo', [
+                'nm_estilo'
+            ]);
+
+        $where = [
+        ];
+
+        if (!empty($filter)) {
+
+            foreach ($filter as $key => $value) {
+
+                if ($value) {
+
+                    if (isset($camposFilter[$key]['mascara'])) {
+
+                        eval("\$value = " . $camposFilter[$key]['mascara'] . ";");
+                    }
+
+                    $where[$camposFilter[$key]['filter']] = '%' . $value . '%';
+                }
+            }
+        }
+
+        $select->where($where)->order(['nm_graduacao DESC']);
+
+        return new \Zend\Paginator\Paginator(new \Zend\Paginator\Adapter\DbSelect($select, $this->getAdapter()));
+    }
+
+    
 }
