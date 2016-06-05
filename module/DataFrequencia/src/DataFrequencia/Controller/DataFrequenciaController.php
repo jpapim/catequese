c346b202db1f7b6a12b75ce478441b16b514b716
Igor Oliveira <igor.oliveira.m@gmail.com>
2016-06-05 13:38:45 -0300

    [Novo] Modulo DataFrequencia - Inicio de Implementacao

---
+++ module/DataFrequencia/src/DataFrequencia/Controller/DataFrequenciaController.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: IGOR
+ * Date: 04/06/2016
+ * Time: 14:30
+ */
+
+class DataFrequencia {
+
+} 
---
+++ module/DataFrequencia/src/DataFrequencia/Entity/DataFrequenciaEntity.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: IGOR
+ * Date: 04/06/2016
+ * Time: 14:30
+ */
+
+namespace DataFrequencia\Entity;
+use Estrutura\Service\AbstractEstruturaService;
+
+class DataFrequenciaEntity extends AbstractEstruturaService {
+
+        protected $id;
+        protected $data;
+} 
