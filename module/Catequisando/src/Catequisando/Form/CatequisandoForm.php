a7440df46205e1509b0b76eac0ecb83a5664f8b1
Igor Oliveira <igor.oliveira.m@gmail.com>
2016-07-16 23:33:19 -0300

    [Atualização] Cadastro Alunos Catequizandos -  Layout (inicio)

--- module/Catequisando/src/Catequisando/Form/CatequisandoForm.php
+++ module/Catequisando/src/Catequisando/Form/CatequisandoForm.php
@@ -11,6 +11,8 @@ namespace Catequisando\Form;
 
 use Estrutura\Form\AbstractForm;
 use Estrutura\Form\FormObject;
+use Etapa\Service\EtapaService;
+use Sacramento\Service\SacramentoService;
 use Zend\InputFilter\InputFilter;
 
 class CatequisandoForm extends  AbstractForm{
@@ -24,18 +26,55 @@ class CatequisandoForm extends  AbstractForm{
 
         $objForm->hidden("id")->required(false)->label("Id");
 
-        #FK - Endereço ######################################################
+        #FK - Endereço
         $objForm->combo("id_endereco", '\Endereco\Service\EnderecoService', 'id', 'nm_logradouro')->required(false)->label("Endereco");
 
-        #FK - Sexo ######################################################
+        #FK - Cidades
+        $objForm->text("cidades")->required(false)->label("Cidade");
+
+        #FK - Sexo
         $objForm->combo("id_sexo", '\Sexo\Service\SexoService', 'id', 'nm_sexo')->required(FALSE)->label("Sexo");
 
-        #FK- Telefone Residencial #######################################
+        #FK- Telefone Residencial
         $objForm->text("id_telefone_residencial")->required(false)->label("Telefone Residencial");
-        #FK- Telefone Celular ###########################################
+
+        #FK- Telefone Celular
         $objForm->text("id_telefone_celular")->required(false)->label("Telefone Celular");
 
-        #FK- Email#####################################################
+        # SACRAMENTOS #
+        #Resgatando as informações da tabela sacaramento
+        #
+        $oSacramento =  new SacramentoService();
+        $colecaoSacramento = $oSacramento->fetchAll();
+        $arrSacramentos=[];
+
+        foreach($colecaoSacramento as $key => $obSacramento){
+            $arrSacramentos[]=[
+                'value'=>$obSacramento->getId(),
+                'id'=>'sacramento'. $key,
+                'label'=>$obSacramento->getNmSacramento(),
+            ];
+        }
+        $objForm->multicheckbox('sacramento', $arrSacramentos)->required(false)->label('Sacramesntos que já recebeu');
+
+        # ETAPA #
+        #Resgatando as informações da tabela sacaramento
+        #
+        $obEtapa =  new EtapaService();
+        $colecaoEtapa = $obEtapa->fetchAll();
+        $arrEtapa=[];
+
+        foreach($colecaoEtapa as $key => $etapa){
+            $arrEtapa[]=[
+                'value'=>$etapa->getId(),
+                'id'=>'etapa'. $key,
+                'label'=>$etapa->getNmEtapa(),
+            ];
+        }
+        $objForm->multicheckbox('etapa', $arrEtapa)->required(false)->label('Etapas já frequentadas');
+
+
+        #FK- Email
         $objForm->email("em_email")->required(true)->label("Email");
         $objForm->email("em_email_confirm")->required(true)->label("Confirme o email")
             ->setAttribute('data-match', '#em_email')
@@ -43,19 +82,12 @@ class CatequisandoForm extends  AbstractForm{
         $objForm->combo("id_email", '\Email\Service\EmailService', 'id', 'em_email')->required(false)->label("Email");
         $objForm->combo('id_situacao','\Situacao\Service\SituacaoService','id','nm_situacao')->required(false)->label("Situacao");
 
-      #FK -  Situacao
-        $objForm->hidden("id_situacao")->required(false)->label("ID Situacao");
-        #FK - Turno
-        $objForm->hidden("id_turno")->required(false)->label("ID Turno");
-        #FK- Movimento Pastoral
-        $objForm->hidden("id_movimento_pastoral")->required(false)->label("ID Movimento Pastoral");
-
         $objForm->text("nm_catequisando")
             ->setAttribute('maxlength','150')
             ->required(true)->label("Nome");
 
         $objForm->date("dt_nascimento")->required(true)->setAttribute('class', 'data')->label("Data de nascimento");
-        $objForm->hidden("dt_ingresso")->required(true)->setAttribute('class', 'data')->label("Data de Ingresso");
+        $objForm->date("dt_ingresso")->required(true)->setAttribute('class', 'data')->label("Data de Ingresso");
 
         $objForm->text("nm_matricula")
             ->setAttribute('maxlength','8')
@@ -63,7 +95,6 @@ class CatequisandoForm extends  AbstractForm{
             ->label("Matricula");
 
         $objForm->textarea("tx_observacao")
-            ->setAttribute('maxlength','8')
             ->required(false)
             ->label("Observações");
 
@@ -82,7 +113,7 @@ class CatequisandoForm extends  AbstractForm{
             ->required(true)
             ->label("Inscrito Estuda?");
 
-        $objForm->checkbox("cs_participante_movimento_pastoral",['Sim','Não'], true)
+        $objForm->radio("cs_participante_movimento_pastoral",['S'=>'Sim','N'=>'Não'])
             ->setAttribute('style',' text-transform: uppercase')
             ->required(true)
             ->label("Participante do Movimento Pastoral");
