8f55605190a47eeb96974677034a802e2da8eeb3
Raimundo de Paula <Raimundodep@gmail.com>
2016-08-01 11:30:39 -0300

    atualizacao documentacao

--- config/application.config.php
+++ config/application.config.php
@@ -72,7 +72,9 @@ return array(
         'SituacaoResponsavel',
         'Catequisando',
 	'Responsavel',
-    ),
+        'CatequistaTurma',
+        'CatequisandoEtapaCursou',
+        ),
     'module_listener_options' => array(
         'module_paths' => array(
             './module',
