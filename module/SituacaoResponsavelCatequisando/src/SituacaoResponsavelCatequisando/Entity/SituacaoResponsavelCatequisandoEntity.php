4db5c552e849487adf2a43647f34ec31e2e052f6
Raimundo de Paula <Raimundodep@gmail.com>
2016-08-11 21:34:22 -0300

    modulo Situacao_Responsavel_Catequisando

---
+++ module/SituacaoResponsavelCatequisando/src/SituacaoResponsavelCatequisando/Entity/SituacaoResponsavelCatequisandoEntity.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace SituacaoResponsavelCatequisando\Entity;
+
+use Estrutura\Service\AbstractEstruturaService;
+
+class SituacaoResponsavelCatequisandoEntity extends AbstractEstruturaService{
+
+        protected $id;
+        protected $id_catequisando;
+        protected $id_situacao_responsavel;
+      
+
+}
