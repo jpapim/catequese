24a5146545e2d00102e3846e1bb014d36935f8c3
Raimundo de Paula <Raimundodep@gmail.com>
2016-08-22 22:59:48 -0300

    tela cadCatequista(funcional mas ainda imcompleto)

--- module/Catequista/src/Catequista/Controller/CatequistaController.php
+++ module/Catequista/src/Catequista/Controller/CatequistaController.php
@@ -36,10 +36,17 @@ class CatequistaController extends AbstractCrudController
     }
     
     public function gravarAction(){
-   
+ 
 // FORMATANDO AS DATAS RECEBIDAS     
-$dateNascimento = \DateTime::createFromFormat('d/m/Y', $this->getRequest()->getPost()->get('dt_nascimento'));
-$dateIngresso = \DateTime::createFromFormat('d/m/Y', $this->getRequest()->getPost()->get('dt_ingresso'));
+#$dateNascimento = \DateTime::createFromFormat('d/m/Y', $this->getRequest()->getPost()->get('dt_nascimento'));
+#$dateIngresso = \DateTime::createFromFormat('d/m/Y', $this->getRequest()->getPost()->get('dt_ingresso'));
+ $dataNascimento = Data::converterDataHoraBrazil2BancoMySQL($this->getRequest()->getPost()->get('dt_nascimento'));
+  $dataIngresso = Data::converterDataHoraBrazil2BancoMySQL($this->getRequest()->getPost()->get('dt_ingresso'));
+
+
+
+
+
 
 
 ###########################- GRAVANDO EMAIL -#######################################################################
@@ -52,7 +59,7 @@ $dateIngresso = \DateTime::createFromFormat('d/m/Y', $this->getRequest()->getPos
         #Alysson - Verifica se já existe este emaill cadastrado para um usuário
         if ($emailService->filtrarObjeto()->count()) {
             $this->addErrorMessage('Email já cadastrado. Faça seu login.');
-           
+           $this->redirect()->toRoute('navegacao', array('controller' => 'catequista-catequista', 'action' => 'index'));
             return FALSE;
         }
            $resultEmail = parent::gravar(
@@ -62,42 +69,170 @@ $dateIngresso = \DateTime::createFromFormat('d/m/Y', $this->getRequest()->getPos
         //GRAVANDO CATEQUISTA
 
     if($resultEmail){
-     $this->addSuccessMessage('Email tabela Inserido/Alterado com sucesso');
+      
+        
+         # Realizando Tratamento do Telefone Residencial
+           $this->getRequest()->getPost()->set('nr_ddd_telefone', \Estrutura\Helpers\Telefone::getDDD($this->getRequest()->getPost()->get('id_telefone_residencial')));
+           $this->getRequest()->getPost()->set('nr_telefone', \Estrutura\Helpers\Telefone::getTelefone($this->getRequest()->getPost()->get('id_telefone_residencial')));
+           $this->getRequest()->getPost()->set('id_tipo_telefone', $this->getConfigList()['tipo_telefone_residencial']);
+           $this->getRequest()->getPost()->set('id_situacao', $this->getConfigList()['situacao_ativo']);
+           $resultTelefoneResidencial = parent::gravar(
+               $this->getServiceLocator()->get('\Telefone\Service\TelefoneService'), new \Telefone\Form\TelefoneForm()
+           );
+            if($resultTelefoneResidencial){
+                # REalizando Tratamento do  Telefone Celular
+                $this->getRequest()->getPost()->set('nr_ddd_telefone', \Estrutura\Helpers\Telefone::getDDD($this->getRequest()->getPost()->get('id_telefone_celular')));
+                $this->getRequest()->getPost()->set('nr_telefone', \Estrutura\Helpers\Telefone::getTelefone($this->getRequest()->getPost()->get('id_telefone_celular')));
+                $this->getRequest()->getPost()->set('id_tipo_telefone', $this->getConfigList()['tipo_telefone_celular']);
+                $this->getRequest()->getPost()->set('id_situacao', $this->getConfigList()['situacao_ativo']);
+                $resultTelefoneCelular = parent::gravar(
+                    $this->getServiceLocator()->get('\Telefone\Service\TelefoneService'), new \Telefone\Form\TelefoneForm()
+                );
+                if($resultTelefoneCelular){
+                    # Grava os dados do Endereco e retorna o ID do Endereco
+                    $cidade =  new \Cidade\Service\CidadeService();
+                    $id_cidade = $cidade->getIdCidadePorNomeToArray($this->getRequest()->getPost()->get('nm_cidade'));
+                    $this->getRequest()->getPost()->set('id_cidade', $id_cidade['id_cidade']);
+                    $this->getRequest()->getPost()->set('nr_cep', \Estrutura\Helpers\Cep::cepFilter($this->getRequest()->getPost()->get('nr_cep')));
+                    $idEndereco = parent::gravar(
+                        $this->getServiceLocator()->get('\Endereco\Service\EnderecoService'),new \Endereco\Form\EnderecoForm()
+                    );
+                
+      
+                
+                if($idEndereco){
+                  
+                    
+###################Cadastro Usuario ainda nao implementado###################################
+         #$this->getRequest()->getPost()->set('nm_usuario', $this->getRequest()->getPost()->get('nm_usuario'));
+         #$this->getRequest()->getPost()->set('id_email', $resultEmail);
+         #$this->getRequest()->getPost()->set('id_sexo', $this->getRequest()->getPost()->get('id_sexo'));
+         #$this->getRequest()->getPost()->set('id_endereco', $idEndereco);
+         #$this->getRequest()->getPost()->set('id_telefone', $resultTelefoneCelular);
+                    
+                # $resultUsuario = parent::gravar(
+                 #       $this->getServiceLocator()->get('\Usuario\ServiceUsuarioService'),new \Usuario\Form\UsuarioForm()
+                  #  );     
+                 #if($resultUsuario){
+ #################################################################################################################                  
+                       
+              #Resgatando id de cidade e atribuindo ao campo id_naturalidade do cadastro de catequista.
+           $id_naturalidade =  $cidade->getIdCidadePorNomeToArray($this->getRequest()->getPost()->get('nm_naturalidade'));
+           $this->getRequest()->getPost()->set('id_naturalidade',$id_naturalidade['id_cidade']);
+               
   // INSERINDO CAMPOS COM FKS DA TABELA CATEQUISTA 
-       $this->getRequest()->getPost()->set('id_usuario', $this->getRequest()->getPost()->get('id_usuario'));
-       $this->getRequest()->getPost()->set('id_endereco', $this->getRequest()->getPost()->get('id_endereco'));
+       #$this->getRequest()->getPost()->set('id_usuario', $this->getRequest()->getPost()->get('id_usuario'));
+       $this->getRequest()->getPost()->set('id_endereco', $idEndereco);
        $this->getRequest()->getPost()->set('id_sexo', $this->getRequest()->getPost()->get('id_sexo'));
         
-        $this->getRequest()->getPost()->set('id_email', $resultEmail); 
+        $this->getRequest()->getPost()->set('id_email', $resultEmail);
+         $this->getRequest()->getPost()->set('id_telefone_residencial', $resultTelefoneResidencial);
+         $this->getRequest()->getPost()->set('id_telefone_celular', $resultTelefoneCelular);
         $this->getRequest()->getPost()->set('id_situacao', $this->getRequest()->getPost()->get('id_situacao'));
+        $this->getRequest()->getPost()->set('id_detalhe_formacao', $this->getRequest()->getPost()->get('id_detalhe_formacao'));
         
         ///INSERINDO CAMPOS DA TABELA CATEQUISTA
         $this->getRequest()->getPost()->set('nm_catequista', $this->getRequest()->getPost()->get('nm_catequista'));
         $this->getRequest()->getPost()->set('nr_matricula', $this->getRequest()->getPost()->get('nr_matricula'));
-        $this->getRequest()->getPost()->set('dt_nascimento', $dateNascimento->format('Y-m-d'));
-        $this->getRequest()->getPost()->set('dt_ingresso', $dateIngresso->format('Y-m-d'));
+        $this->getRequest()->getPost()->set('dt_nascimento', $dataNascimento);
+        $this->getRequest()->getPost()->set('dt_ingresso', $dataIngresso);
         $this->getRequest()->getPost()->set('tx_observacao', $this->getRequest()->getPost()->get('tx_observacao'));
-        $this->getRequest()->getPost()->set('ds_situacao', $this->getRequest()->getPost()->get('ds_situacao'));
-        $this->getRequest()->getPost()->set('cs_coodenador', $this->getRequest()->getPost()->get('cs_coordenador'));
+       # $this->getRequest()->getPost()->set('ds_situacao', $this->getRequest()->getPost()->get('ds_situacao'));
+       # $this->getRequest()->getPost()->set('cs_coodenador', $this->getRequest()->getPost()->get('cs_coordenador'));
         
         //GRAVANDO CAMPOS CATEQUISTA,PARAMETRO (FORMULARIO ALTERNATIVO SEM COMBOS)
-           parent::gravar(
+           $resultCatequista= parent::gravar(
                             $this->getServiceLocator()->get('\Catequista\Service\CatequistaService'), new \Catequista\Form\CatequistaDetalheForm()
             );
-    
-    $this->addSuccessMessage('Registro Inserido/Alterado com sucesso');
-    $this->redirect()->toRoute('navegacao', array('controller' => 'catequista-catequista', 'action' => 'index'));
-  
+           
+     
+           
+            if($resultCatequista){
+                                #Resgatando e inserindo manualmente na tabela catequista_etapa_atuacao as ids das etapas ja realizadas.
+                                $arrEtapa =  $this->getRequest()->getPost()->get('arrEtapa');
+                                foreach($arrEtapa as $etapa){
+                                    $this->getRequest()->getPost()->set('id_etapa', $etapa);
+                                    $this->getRequest()->getPost()->set('id_catequista', $resultCatequista);
+                                    $this->getRequest()->getPost()->set('dt_cadastro', date('Y-m-d H:m:s'));
+                                    #Chamo o metodo para gravar os dados na tabela.
+                                    parent::gravar(
+                                        $this->getServiceLocator()->get('\CatequistaEtapaAtuacao\Service\CatequistaEtapaAtuacaoService'), new \CatequistaEtapaAtuacao\Form\CatequistaEtapaAtuacaoForm()
+                            );
+                            
+                                }
+                                $status = true;
+                            }
+                        
+                }}
+                }
+            
+           if ($status ) {
+               $this->addSuccessMessage('Parabéns! Catequizando cadastrado com sucesso.');
+               $this->redirect()->toRoute('navegacao', array('controller' => 'catequista-catequista', 'action' => 'index'));
+           }
+            else{
+                $this->addErrorMessage('Processo não pode ser concluido.');
+                $this->redirect()->toRoute('navegacao', array('controller' => 'catequista-catequista', 'action' => 'cadastro'));
+            }
     }}
+   
+           
+           
+           
+           
+           
+    
+   
 
   public function cadastroAction()
     { // funnção alterar
         return parent::cadastro($this->service, $this->form);
     }
-    public function excluirAction()
+    public function excluirAction($option = null)
     {
-        return parent::excluir($this->service, $this->form);
-}
+        #xd($option);
+        #ID_CATEQUISANDO
+        $id = Cript::dec($this->params('id'));
+        if(!empty($option)){
+            $id = Cript::dec($option);
+        }
+        if(isset($id) && $id){
+            $obcatequista=  new \Catequista\Service\CatequistaService();
+            $arrCatequista =$obcatequista->getCatequistaToArray($id);
+        
+
+         ##############Excluindo dados da tabela filha###############
+            $obCatequistaTurmaService = new \CatequistaTurma\Service\CatequistaTurmaService();
+            $obCatequistaTurmaService->setIdCatequista($id);
+            $obCatequistaTurmaService->excluir();
+            
+            $obCatequistaEtapaAtuacaoService = new \CatequistaEtapaAtuacao\Service\CatequistaEtapaAtuacaoService();
+            $obCatequistaEtapaAtuacaoService->setIdCatequista($id);
+            $obCatequistaEtapaAtuacaoService->excluir();
+           
+    
+            
+           $obCatequistaService = new \Catequista\Service\CatequistaService();
+           $obCatequistaService->setIdCatequista($id);
+           $obCatequistaService->excluir();
+            
+           $obEnderecoService = new \Endereco\Service\EnderecoService();
+           $obEnderecoService->setIdCatequista($id);
+           $obEnderecoService->excluir();
+            
+           $obTelResidencial =  new \Telefone\Service\TelefoneService();
+           $obTelResidencial->setId($arrCatequisando['id_telefone_residencial']);
+           $obTelResidencial->excluir();
+            
+           $obTelCelular =  new \Telefone\Service\TelefoneService();
+           $obTelCelular->setId($arrCatequisando['id_telefone_celular']);
+           $obTelCelular->excluir();
+        
+           $objEmail = new \Email\Service\EmailService();
+           $objEmail->setId($arrCatequisando['id_email']);
+           $objEmail->excluir();
+    
+        }}
  public function indexPaginationAction()
     {// funcao paginacao
         //http://igorrocha.com.br/tutorial-zf2-parte-9-paginacao-busca-e-listagem/4/
@@ -124,13 +259,7 @@ $dateIngresso = \DateTime::createFromFormat('d/m/Y', $this->getRequest()->getPos
                 'filter' => "catequista.tx_observacao LIKE ?",
             ],
 
-                '5' => [
-                'filter' => "catequista.ds_situacao LIKE ?",
-            ],
-            
-                '6' => [
-                'filter' => "catequista.cs_coordenador LIKE ?",
-            ],
+        
             
             
         ];
--- module/Catequista/src/Catequista/Form/CatequistaDetalheForm.php
+++ module/Catequista/src/Catequista/Form/CatequistaDetalheForm.php
@@ -4,6 +4,7 @@ namespace Catequista\Form;
 
 use Estrutura\Form\AbstractForm;
 use Estrutura\Form\FormObject;
+use Etapa\Service\EtapaService;
 use Zend\InputFilter\InputFilter;
 
 
@@ -17,30 +18,73 @@ class CatequistaDetalheForm extends AbstractForm {
              // CAMPOS DE FKS
              
        $objForm = new FormObject('catequistadetalheform',$this,$this->inputFilter);
-       $objForm->hidden("id")->required(false)->label("Id");
+      $objForm->hidden("id")->required(false)->label("Id");
       //##############----BUSCANDO CAMPOS-- #################### 
-       $objForm->hidden("id_usuario")->required(false)->label("Usuario");
-       $objForm->hidden("id_cidade")->required(false)->label("Cidade");
-       $objForm->hidden("id_endereco")->required(false)->label("Endereco");
-       $objForm->hidden("id_sexo")->required(FALSE)->label("Sexo");
-       //$objForm->hidden("id_naturalidade")->required(false)->label("Nome completo");
-      
-        $objForm->hidden("id_email")->required(false)->label("Email");
-        $objForm->hidden('id_situacao')->required(false)->label("Situacao");      
+       #$objForm->combo("id_usuario", '\Usuario\Service\UsuarioService', 'id', 'nm_usuario')->required(false)->label("Usuario");
+       $objForm->combo("id_cidade", '\Cidade\Service\CidadeService', 'id', 'nm_cidade')->required(false)->label("Cidade");
+       $objForm->combo("id_endereco", '\Endereco\Service\EnderecoService', 'id', 'nm_logradouro')->required(false)->label("Endereco");
+       $objForm->combo("id_sexo", '\Sexo\Service\SexoService', 'id', 'nm_sexo')->required(FALSE)->label("Sexo");
+       $objForm->hidden("id_email")->required(false);
        
-        // $objForm->text("id_telefone_residencial")->required(false)->label("Nome completo");
-       // $objForm->text("id_telefone_celular")->required(false)->label("Nome completo");
-      //######################################################################################### 
-  
+        $objForm->combo('id_situacao','\Situacao\Service\SituacaoService','id','nm_situacao')->required(false)->label("Situacao");      
+       #$objForm->combo("id_detalhe_formacao", '\DetalheFormacao\Service\DetalheFormacaoService', 'id', 'id_detalhe_formacao')->required(false)->label("Formação Acadêmica");
+        ##### Endereço ######
+        $objForm->hidden("id_endereco")->required(false);
+        $objForm->text("nm_logradouro")->required(false)->label("Logradouro");
+        $objForm->text("nr_numero")->required(true)->label("Número");
+        $objForm->text("nm_complemento")->required(true)->label("Complemento");
+        $objForm->text("nm_bairro")->required(true)->label("Bairro");
+        $objForm->cep("nr_cep")->required(true)->label("Cep");
+        #FK - Naturalidade
+        $objForm->text("nm_naturalidade")->required(false)->label("Naturalidade");
+        #FK - Cidades
+        $objForm->text("nm_cidade")->required(false)->label("Cidade");
+        
+        
+        #FK- Telefone Residencial
+        $objForm->hidden("id_telefone_residencial")->required(false);
+        $objForm->telefone("telefone_residencial")->setAttribute('class', 'telefone')->required(false)->label("Telefone Residencial");
+        #FK- Telefone Celular
+        $objForm->telefone("telefone_celular")->setAttribute('class', 'telefone')->required(false)->label("Telefone Celular");
+        $objForm->hidden("id_telefone_celular")->required(false);
+
+       
+        # ETAPA #
+        #Resgatando as informações da tabela etapa
+        #
+        $obEtapa =  new EtapaService();
+        $colecaoEtapa = $obEtapa->fetchAll();
+        $arrEtapa=[];
+        if(isset($options['arrEtapa']) && $options['arrEtapa']){
+            foreach($colecaoEtapa as $key => $etapa){
+                $arrEtapa[]=[
+                    'value'=>$etapa->getId(),
+                    'name'=>'etapa['.$etapa->getId().']',
+                    'label'=>$etapa->getNmEtapa(),
+                    'selected'=>in_array($etapa->getId(),$options['arrEtapa'])? true: false,
+                ];
+            }
+        }else{
+            foreach($colecaoEtapa as $key => $etapa){
+                $arrEtapa[]=[
+                    'value'=>$etapa->getId(),
+                    'name'=>'etapa['.$etapa->getId().']',
+                    'label'=>$etapa->getNmEtapa(),
+                ];
+            }
+        }
+        $objForm->multicheckbox('arrEtapa', $arrEtapa)->required(false)->label('Etapas que já Atuou');
+        
+
 ////CAMPOS DA TABELA
 
-           $objForm->hidden("nm_catequista")->required(true)->label("Nome completo");
-           $objForm->hidden("nr_matricula")->required(true)->label("numero matricula");
+           $objForm->text("nm_catequista")->required(true)->label("Nome completo");
+           $objForm->text("nr_matricula")->required(true)->label("numero matricula");
            $objForm->date("dt_nascimento")->required(true)->label("Data de nascimento");
            $objForm->date("dt_ingresso")->required(true)->label("Data de ingresso");
-           $objForm->hidden("tx_observacao")->required(true)->label("observacao");
-           $objForm->hidden("ds_situacao")->required(true)->label("Descricao da situacao");
-           $objForm->hidden("cs_coordenador")->required(true)->label("cs cordenador");
+           $objForm->textarea("tx_observacao")->required(true)->label("observacao");
+           #$objForm->text("ds_situacao")->required(false)->label("Descricao da situacao");
+          # $objForm->text("cs_coordenador")->required(false)->label("cs cordenador");
 
         $this->formObject = $objForm;
     }
