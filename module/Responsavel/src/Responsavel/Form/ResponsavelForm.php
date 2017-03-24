6ceccd71d2e6eb0c859c986d8fc9b43e08a57e92
yves <yveslopo@gmail.com>
2017-03-24 14:29:44 -0300

    Adicionar quais são os campos obrigatórios no cadastro de Responsáveis.

--- module/Responsavel/src/Responsavel/Form/ResponsavelForm.php
+++ module/Responsavel/src/Responsavel/Form/ResponsavelForm.php
@@ -20,16 +20,16 @@ class ResponsavelForm extends AbstractForm {
        $objForm->hidden("id")->required(false)->label("Id");
       //##############----BUSCANDO CAMPOS-- #################### 
 
-       $objForm->combo("id_sexo", '\Sexo\Service\SexoService', 'id', 'nm_sexo')->required(FALSE)->label("Sexo");
+       $objForm->combo("id_sexo", '\Sexo\Service\SexoService', 'id', 'nm_sexo')->required(true)->label("*Sexo");
 
-     $objForm->email("em_email")->required(true)->label("E-mail");
-            $objForm->email("em_email_confirm")->required(true)->label("Confirme o E-mail")
+     $objForm->email("em_email")->required(true)->label("*E-mail");
+            $objForm->email("em_email_confirm")->required(true)->label("*Confirme o E-mail")
                 ->setAttribute('data-match', '#em_email')
                 ->setAttribute('data-match-error', 'E-mails não correspondem');
         $objForm->combo("id_email", '\Email\Service\EmailService', 'id', 'em_email')->required(false)->label("Email");
         $objForm->combo('id_situacao','\Situacao\Service\SituacaoService','id','nm_situacao')->required(false)->label("Situação");
       $objForm->combo("id_profissao", '\Profissao\Service\ProfissaoService', 'id', 'nm_profissao')->required(false)->label("Profissão");
-      $objForm->combo("id_movimento_pastoral", '\MovimentoPastoral\Service\MovimentoPastoralService', 'id', 'nm_movimento_pastoral')->required(false)->label("Movimento Pastoral");
+      $objForm->combo("id_movimento_pastoral", '\MovimentoPastoral\Service\MovimentoPastoralService', 'id', 'nm_movimento_pastoral')->required(true)->label("*Movimento Pastoral");
      
         //#########################################################################################
 
@@ -37,17 +37,17 @@ class ResponsavelForm extends AbstractForm {
             $objForm->radio("cs_participa_movimento_pastoral",['S'=>'Sim','N'=>'Não'])
             ->setAttribute('style',' text-transform: uppercase')
             ->required(true)
-            ->label("Participa de Movimento Pastoral");
+            ->label("*Participa de Movimento Pastoral");
   
 ////CAMPOS DA TABELA
 
-           $objForm->text("nm_responsavel")->required(true)->label("Nome Completo");
+           $objForm->text("nm_responsavel")->required(true)->label("*Nome Completo");
            $objForm->textarea("tx_observacao")->required(true)->label("Observação");
 
 
         //Campo de TELEFONE RESIDENCIAL E TELEFONE CELULAR
-         $objForm->telefone("id_telefone_residencial")->setAttribute('class', 'telefone')->required(true)->label("Telefone Residencial");
-         $objForm->telefone("id_telefone_celular")->setAttribute('class', 'telefone')->required(true)->label("Telefone Celular");
+         $objForm->telefone("id_telefone_residencial")->setAttribute('class', 'telefone')->required(true)->label("*Telefone Residencial");
+         $objForm->telefone("id_telefone_celular")->setAttribute('class', 'telefone')->required(true)->label("*Telefone Celular");
 
 
         $this->formObject = $objForm;
